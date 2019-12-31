1b8dd0c87f4eee156366eefd96a515cc73fb7d46
oguzhan <oguzhanaknc06@gmail.com>
2019-12-31 08:45:18 +0300

    yönlendirme hatası düzelttim

--- components/up.js
+++ components/up.js
@@ -7,7 +7,7 @@ const Uparea = () => (
       <h1 className='hero-title'>Oğuzhan A.</h1>
 
       <div className='hero-main-links'>
-        <Link href='/index'>
+        <Link href='http://oguzhanaknc.herokuapp.com/'>
           <a className='main-link'>Anasayfa</a>
         </Link>
         <Link href='/about'>
