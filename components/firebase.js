3539167bc586a67ff0fa09aea1f271dcb0002cd2
Phoique <official.phoique@gmail.com>
2020-02-08 21:04:06 +0300

    dotenv settings and edit firebase config

--- components/firebase.js
+++ components/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 import "firebase/auth";
 var firebaseConfig = {
-  apiKey: "AIzaSyAM_gGqUxk0-faWvJP4TKU7wEvNN-vIac4",
-  authDomain: "oguzhanakinciblog.firebaseapp.com",
-  databaseURL: "https://oguzhanakinciblog.firebaseio.com",
-  projectId: "oguzhanakinciblog",
-  storageBucket: "oguzhanakinciblog.appspot.com",
-  messagingSenderId: "937758449479",
-  appId: "1:937758449479:web:ef65942a657dd4d94eb662"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATEBASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID
 };
 // Initialize Firebase
 if (!firebase.apps.length) {
